540eb538427c7d1e95715b13861059f29865d3e0
Region60 <_max_kot@mail.ru>
2020-08-20 21:54:37 +0300

    'dell1'

--- keys2/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URI: "mongodb+srv://maksim:8u2upvDe0W1dp945@cluster0-mjkka.mongodb.net/request",
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: '_max_kot@mail.ru',
-    BASE_URL: 'http://localhost:3000'
-}
