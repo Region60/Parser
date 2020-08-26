c74bd46b8960da1491ddeb52ed981e77cd89e408
Region60 <_max_kot@mail.ru>
2020-08-26 22:43:28 +0300

    'ignoreKeys'

--- keys/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URI: "mongodb+srv://maksim:8u2upvDe0W1dp945@cluster0-mjkka.mongodb.net/request",
-    SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY:'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',
-    EMAIL_FROM: '_max_kot@mail.ru',
-    BASE_URL: 'http://localhost:3000'
-}
